b12fd9e2b0623a8013908521af36010bf04f4b52
Nirant Kasliwal <nirant.bits+githubmail@gmail.com>
2014-07-06 16:49:03 +0530

    Add India analysis. Finds the happiest state in India

--- happiest_state_India.py
+++ happiest_state_India.py
@@ -1,65 +1,33 @@
 import sys, json
 
-happyState = {}
-states = {
-        'AK': 'Alaska',
-        'AL': 'Alabama',
-        'AR': 'Arkansas',
-        'AS': 'American Samoa',
-        'AZ': 'Arizona',
-        'CA': 'California',
-        'CO': 'Colorado',
-        'CT': 'Connecticut',
-        'DC': 'District of Columbia',
-        'DE': 'Delaware',
-        'FL': 'Florida',
-        'GA': 'Georgia',
-        'GU': 'Guam',
-        'HI': 'Hawaii',
-        'IA': 'Iowa',
-        'ID': 'Idaho',
-        'IL': 'Illinois',
-        'IN': 'Indiana',
-        'KS': 'Kansas',
-        'KY': 'Kentucky',
-        'LA': 'Louisiana',
-        'MA': 'Massachusetts',
-        'MD': 'Maryland',
-        'ME': 'Maine',
-        'MI': 'Michigan',
-        'MN': 'Minnesota',
-        'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
-        'MS': 'Mississippi',
-        'MT': 'Montana',
-        'NA': 'National',
-        'NC': 'North Carolina',
-        'ND': 'North Dakota',
-        'NE': 'Nebraska',
-        'NH': 'New Hampshire',
-        'NJ': 'New Jersey',
-        'NM': 'New Mexico',
-        'NV': 'Nevada',
-        'NY': 'New York',
-        'OH': 'Ohio',
-        'OK': 'Oklahoma',
-        'OR': 'Oregon',
-        'PA': 'Pennsylvania',
-        'PR': 'Puerto Rico',
-        'RI': 'Rhode Island',
-        'SC': 'South Carolina',
-        'SD': 'South Dakota',
-        'TN': 'Tennessee',
-        'TX': 'Texas',
-        'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming'
-}
+happyStates = {}
+
+class happyState(object):
+    def __init__(self):
+        self.stateName = ""
+        self.count = 0
+        self.score = 0.0
+
+    # def setName(self, name):
+        # self.stateName = name
+
+    def updateCount(self):
+        self.count += 1.0
+
+    def updateScore(self, newScore):
+        self.score += newScore
+
+def stateSort(data):
+    for i in xrange(len(data)):
+        key = data[i].getStateCode()
+        try:
+            happyStates[key] 
+        except KeyError:
+            happyStates[key] = happyState()
+            happyStates[key].stateName = key
+        finally:
+            happyStates[key].score += data[i].getScore()
+            happyStates[key].updateCount()
 
 class Tweet:
     '''Stores the necessary data for each tweet'''
@@ -101,6 +69,7 @@ class tweetScore(object):
     def sentFile(self, sent_file):
         afinnfile = open(sent_file)
         for line in afinnfile:
+            # print line
             term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
             self.scores[term] = int(score)  # Convert the score to an integer.
 
@@ -146,7 +115,7 @@ class readTweet(object):
                     tweet = pyResponse['text'].encode('utf-8')
                     loc = city.split(", ")[0]
                     stateCode = city.split(", ")[1]
-                    print city
+                    # print city
                     # if stateCode =='USA':
                     #     stateCode = self.substitute(loc)
                     # print stateCode, tweet
@@ -159,16 +128,6 @@ class readTweet(object):
                 pass
         return self.data
           
-def stateSort(data):
-    '''simply calculates the happiness for each state by iterating through the state dict and data from Twitter'''
-    for key in states.keys():
-        for i in xrange(len(data)):
-            if data[i].getStateCode() == key:
-                try:
-                    happyState[key] += data[i].getScore()
-                except:
-                    happyState[key] = data[i].getScore()
-
 def main():
     sent_file = str(sys.argv[1])
     tweet_file = str(sys.argv[2])
@@ -177,13 +136,13 @@ def main():
     # print type(tw.data)
     ts = tweetScore()
     ts.calcTweetScore(sent_file, tw.data)
-    # for i in xrange(len(tw.data)):
-    #     print tw.data[i].getStateCode(), tw.data[i].getScore(), tw.data[i].getTweet()
     
-    # stateSort(tw.data)
-    # maximum = -9999999999999
-    # for key in happyState.keys(): # finds the maximum happiness in a state
-    #     # print key, happyState[key]
+    hs = happyState()
+    stateSort(tw.data)
+    for key in happyStates.keys(): # finds the maximum happiness in a state
+        if happyStates[key].count > 25:
+            print key, "\t", (happyStates[key].score)/(happyStates[key].count), "\t", happyStates[key].count
+            # print key, happyStates[key].count
     #     if happyState[key] > maximum: 
     #         maximum = happyState[key]
 
@@ -192,4 +151,7 @@ def main():
     #         print key
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv)==3:
+        main()
+    else:
+        print 'Please enter sentiment file and then tweet file'
